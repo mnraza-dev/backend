d2f59e68d5fa11e45518aad5d837393170226b40
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-02 00:00:27 +0530

    Refactor user schema formatting for improved readability in userModels.js

--- models/userModels.js
+++ models/userModels.js
@@ -1,27 +1,32 @@
 import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        trim: true,
-        required: true,
-        unique: true,
-        lowercase: true
-    },
-    password: {
-        type: String,
-        required: true,
-        trim:true
-    },
-    isVerified:{
-        type: Boolean,
-        default: false
-    }
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  roles: {
+    type: [String],
+    enum: [" user", "admin"],
+    default: ["user"],
+  },
 });
 
 const User = mongoose.model("User", userSchema);
