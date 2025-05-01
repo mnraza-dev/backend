fe773560e795e0680f1fb7f369688ea337d5bced
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-01 23:49:23 +0530

    Configure CORS middleware with specific options for origin, methods, and allowed headers

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(cors());
+app.use(cors({
+    origin: process.env.ORIGIN,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
