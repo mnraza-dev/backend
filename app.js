5a139c318659d996e1edde6340389ed39148ae1b
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-01 23:50:15 +0530

    Update CORS middleware to include optionsSuccessStatus for improved compatibility

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors({
     origin: process.env.ORIGIN,
+    
+    optionsSuccessStatus: 200,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
