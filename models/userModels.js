f54e6734e5b5b69be1e73f1874c2f974cb877b70
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-01 23:59:11 +0530

    Fix formatting by adding missing trim property in user schema fields

--- models/userModels.js
+++ models/userModels.js
@@ -4,9 +4,11 @@ const userSchema = mongoose.Schema({
     name: {
         type: String,
         required: true,
+        trim: true
     },
     email: {
         type: String,
+        trim: true,
         required: true,
         unique: true,
         lowercase: true
@@ -14,6 +16,7 @@ const userSchema = mongoose.Schema({
     password: {
         type: String,
         required: true,
+        trim:true
     },
     isVerified:{
         type: Boolean,
