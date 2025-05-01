9ab67b554b8e640d28feafae87867e4bd6c31468
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-01 23:58:31 +0530

    Add isVerified field to user schema in userModels.js

--- models/userModels.js
+++ models/userModels.js
@@ -9,11 +9,16 @@ const userSchema = mongoose.Schema({
         type: String,
         required: true,
         unique: true,
+        lowercase: true
     },
     password: {
         type: String,
         required: true,
     },
+    isVerified:{
+        type: Boolean,
+        default: false
+    }
 });
 
 const User = mongoose.model("User", userSchema);
