62583c85402158ca99b9bceb6e78dea34d1513a8
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-01 23:56:16 +0530

    Initialize passport middleware in app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(cors({
    }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(passport.initialize());
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
