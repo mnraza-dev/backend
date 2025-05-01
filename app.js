e58d8ffcf708e3fe60452e845d9c6cc8c380ddda
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-01 23:45:51 +0530

    Implement code changes to enhance functionality and improve performance

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log("Server running on port 8080");
+});
